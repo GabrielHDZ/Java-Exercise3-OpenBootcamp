c1579fdb80d617cb5e640020eb263b2f0ae971aa
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-05-14 00:27:12 -0500

    first commit in Java code

---
+++ src/App.java
@@ -0,0 +1,24 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //variables camelCase
+        String v1="";
+        
+        char v4='D';
+        
+        boolean v6=true;
+        final String v7="data";
+        //numbers
+        //integer
+        int v2=2147483647;
+        byte v8=100;
+        short v9=32767;
+        long v10=9223372036854775807;
+        //floating
+        float v3=15.99f;
+        double v5=19.99d;
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println("Hello, World!");
+    }
+}
